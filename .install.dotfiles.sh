437f6b83acdd615a15ba9f83c323e363697f24e3
Matt Edmonston <matt@ethercreative.co.uk>
2019-03-08 12:10:05 +0000

    Update .install.dotfiles.sh

--- .install.dotfiles.sh
+++ .install.dotfiles.sh
@@ -9,3 +9,4 @@ brew install $(cat ~/.brewcask)
 git clone https://github.com/powerline/fonts.git --depth=1 ~/.fonts
 ~/.fonts/install.sh
 rm -rf ~/.fonts
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim

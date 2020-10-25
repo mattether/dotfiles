82deef7a44b01e2d202f9521b02173c3ea74606d
Matt Edmonston <matt.edmonston@gmail.com>
2020-10-25 15:04:48 +0000

    Update .install.dotfiles.sh

--- .install.dotfiles.sh
+++ .install.dotfiles.sh
@@ -10,5 +10,5 @@ git clone https://github.com/powerline/fonts.git --depth=1 ~/.fonts
 ~/.fonts/install.sh
 rm -rf ~/.fonts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall "+CocInstall coc-json coc-css coc-phpls coc-html coc-tsserver coc-yaml coc-emmet coc-sql" +qall
+vim +PlugInstall "+CocInstall coc-json coc-css coc-phpls coc-html coc-tsserver coc-yaml coc-emmet coc-sql coc-tailwindcss" +qall
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
